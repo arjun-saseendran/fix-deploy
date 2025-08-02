5b97beb54469f501e3838ce7d3f1e03f633d83f0
arjun-saseendran <arjunsaseendran@icloud.com>
2025-08-02 12:58:21 +0530

    deploy fix

---
+++ src/components/Popularitemlist.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import SelectedPopularitem from "../Routes/SelectedPopularitem";
+
+
+function Popularitemlist(props) {
+   
+     const navigate = useNavigate();
+     const handleOrder = () => {
+      
+        navigate(`/SelectedPopularitem/${props.id}`, { state: props });
+     };
+    
+
+    return (
+        <article className="container flex-col ">
+              <div className="w-40 h-35 ">
+                <img className="rounded-lg" src={props.snap} alt="Cheese Burger" />
+              </div>
+              <div className="pt-5">
+                <h3 className="font-bold ">{props.dish}</h3>
+              </div>
+              <div>
+                <span className="flex flex-row text-orange-400 "><img src="./icons/map-marker-alt.png" alt="Location icon" />{props.location}</span>
+              </div>
+              <div>
+                <p className="font-bold">{props.price}</p>
+              </div>
+              <button onClick={handleOrder} className="bg-orange-500 px-5 py-2 text-white rounded-md w-40">Order Now</button>
+            </article>
+    );
+}
+export default Popularitemlist;
---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './routes/Layout.jsx';
+import SelectedPopularitem from './routes/SelectedPopularitem.jsx';
+
+
+
+
+createRoot(document.getElementById('root')).render(
+  
+  <BrowserRouter>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<App />} />
+        <Route path="/SelectedPopularitem/:id" element={<SelectedPopularitem />}/>
+        
+      </Route>
+    </Routes>
+  </BrowserRouter>
+  
+)
